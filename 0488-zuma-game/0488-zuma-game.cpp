45ccf91e1b50561155f0164586255146298aed8f
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-07-16 01:43:08 +0530

    Time: 1460 ms (6.35%), Space: 226.5 MB (11.64%) - LeetHub

---
+++ 0488-zuma-game/0488-zuma-game.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    unordered_map<string, int> memo;
+
+    string removeConsecutive(string board) {
+        int i = 0;
+        while (i < board.size()) {
+            int j = i;
+            while (j < board.size() && board[i] == board[j]) j++;
+            if (j - i >= 3) {
+                board = board.substr(0, i) + board.substr(j);
+                i = 0; // restart after removal
+            } else {
+                i++;
+            }
+        }
+        return board;
+    }
+
+    int dfs(string board, unordered_map<char, int>& hand) {
+        board = removeConsecutive(board);
+        if (board.empty()) return 0;
+
+        string key = board + "#" + serialize(hand);
+        if (memo.count(key)) return memo[key];
+
+        int res = INT_MAX;
+
+        for (int i = 0; i < board.size(); ++i) {
+            for (auto& [c, count] : hand) {
+                if (count == 0) continue;
+
+                // Only insert if it helps: either same char or diff in between same chars
+                if (c == board[i] || (i > 0 && board[i - 1] == board[i] && board[i - 1] != c)) {
+                    hand[c]--;
+                    string newBoard = board.substr(0, i) + c + board.substr(i);
+                    int steps = dfs(newBoard, hand);
+                    if (steps != -1) {
+                        res = min(res, steps + 1);
+                    }
+                    hand[c]++;
+                }
+            }
+        }
+
+        return memo[key] = (res == INT_MAX ? -1 : res);
+    }
+
+    string serialize(unordered_map<char, int>& hand) {
+        string result;
+        for (char c : {'R', 'Y', 'B', 'G', 'W'}) {
+            result += string(1, c) + to_string(hand[c]);
+        }
+        return result;
+    }
+
+    int findMinStep(string board, string hand) {
+        unordered_map<char, int> handMap;
+        for (char c : hand) handMap[c]++;
+        return dfs(board, handMap);
+    }
+};
