0e02075feed9ec6da2053d9ad8d0c1d79cfe390c
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-07-23 21:17:05 +0530

    Time: 28 ms (80.09%), Space: 28.2 MB (56.96%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,59 +1,56 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        vector<char> v1,v2;
         bool f;
-        if(x>y)  f=1;
+        if(x>y) f=1;
         else f=0;
-        int ans=0;
 
-        for(auto a:s){
+        vector<char> v1,v2;
+        int cnt=0;
+
+        for(auto& a:s){
             if(!v1.empty()){
                 if(f){
                     if(v1.back()=='a' && a=='b'){
-                        ans+=x;
-                        v1.pop_back();
-                    }
-                    else{
+                         cnt+=x;
+                         v1.pop_back();
+                    }else{
                         v1.push_back(a);
                     }
-                }
-                else{
+                }else{
                     if(v1.back()=='b' && a=='a'){
-                        ans+=y;
+                        cnt+=y;
                         v1.pop_back();
-                    }
-                    else{
+                    }else{
                         v1.push_back(a);
-                    }                            
+                    }
                 }
+            }else{
+                v1.push_back(a);
             }
-            else v1.push_back(a);
         }
-        for(auto a:v1){
+        for(auto& a:v1){
             if(!v2.empty()){
                 if(!f){
                     if(v2.back()=='a' && a=='b'){
-                        ans+=x;
-                        v2.pop_back();
-                    }
-                    else{
+                         cnt+=x;
+                         v2.pop_back();
+                    }else{
                         v2.push_back(a);
                     }
-                }
-                else{
+                }else{
                     if(v2.back()=='b' && a=='a'){
-                        ans+=y;
+                        cnt+=y;
                         v2.pop_back();
-                    }
-                    else{
+                    }else{
                         v2.push_back(a);
-                    }                            
+                    }
                 }
+            }else{
+                v2.push_back(a);
             }
-            else v2.push_back(a);
         }
 
-        return ans;
+        return cnt;
     }
 };
