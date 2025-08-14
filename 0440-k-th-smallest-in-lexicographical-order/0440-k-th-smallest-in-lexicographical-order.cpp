2ecfd0b4232d0ece2e1ada4049f174736413d64d
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-08-14 10:46:16 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.55%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
 
     int findKthNumber(int n, int k) {
-       int curr=1;
-       k--;
+        long long curr=1;
+        k--;
 
-       while(k>0){
-            long long steps=countStep(n,curr,curr+1);
-            if(steps<=k){
-                curr+=1; // find steps bw next two sibling
-                k-=steps;
+        while(k>0){
+            long long steps=findsteps(n,curr,curr+1);
+
+            if(steps>k){
+                curr=curr*10;
+                k--;
             }else{
-                curr*=10; // go deeper inside same tree;
-                k-=1;
+                curr+=1;
+                k-=steps;
             }
-       }
 
-       return curr;
+        }
+        return curr;
     }
+     long long findsteps(int n,long long curr,long long next){
+            long long steps=0;
 
-    long long countStep(long long n,long long curr,long long next){
-    long long steps=0;
+            while(curr<=n){
+                steps+=min((n+1)*1LL,next)-curr;
+                curr*=10;
+                next*=10;
+            }
 
-    while(curr<=n){
-        steps+=min(n+1,next)-curr;
-        curr*=10;
-        next*=10;
-    }
-    return steps;
-}
+            return steps;
+        }
 };
