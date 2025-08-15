5d47df9348aa9162b81f06d8a0e5151141047fd8
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-08-15 09:07:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.41%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -4,14 +4,9 @@ public:
 
         if(n<=0) return false;
 
-        while(n>0){
-            if(n==1) return true;
-            if(n%4!=0){
-                return false;
-            }
-            n=n/4;
-        }
+        if(n==1) return true;
 
-        return true;
+        return n%4==0 && isPowerOfFour(n/4);
+        
     }
 };
