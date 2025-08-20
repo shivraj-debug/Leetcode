bf3c5a5a080d4150d3c9a8da6899542112a537ef
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-08-20 12:56:05 +0530

    Time: 4 ms (73.98%), Space: 30.4 MB (48.39%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -7,29 +7,29 @@ public:
         n=matrix.size();
         m=matrix[0].size();
 
-        dp.resize(n+1,vector<int>(m+1,-1));
+        dp.resize(n,vector<int>(m,0));
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                res+=rec(i,j,matrix);
+            dp[i][0]=matrix[i][0];
+            res+=matrix[i][0];
+        }
+
+        for(int i=1;i<m;i++){
+            dp[0][i]=matrix[0][i];
+            res+=matrix[0][i];
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if (matrix[i][j] == 1) {
+                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+                } else {
+                    dp[i][j] = 0;
+                }
+                res += dp[i][j];
             }
         }
 
         return res;
-    }
-
-    int rec(int i,int j,const vector<vector<int>>& mat){
-        if(i==n-1 || j==m-1) return mat[i][j];
-       
-        if(mat[i][j]==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int right=0,dia=0,bottom=0;
-
-        right=rec(i,j+1,mat);
-        dia=rec(i+1,j+1,mat);
-        bottom=rec(i+1,j,mat);
-
-        return dp[i][j]=mat[i][j]+min({right,dia,bottom});
     }
 };
