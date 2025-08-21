678707596d52ecc22a584ea2ab503985fa62d1ec
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-08-21 23:01:22 +0530

    Time: 59 ms (15.65%), Space: 7.8 MB (94.41%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> arr(n, 0);
+        string ans = "";
+        for(int i = 1; i <= n; i++) {
+            arr[i-1] = i;
+        }
+
+        for(int i = 1; i < k; i++) {
+            next_permutation(arr.begin(), arr.end());
+        }
+
+
+        for(auto i : arr) cout << i << " ";
+
+        for(int i = 0; i < arr.size(); i++) {
+
+            ans.push_back(arr[i]+'0');
+        }
+
+        return ans; 
+    }
+};
