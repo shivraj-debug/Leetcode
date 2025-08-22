7fae0819e84f38e32f375f30326de7e2a2034fed
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-08-22 11:17:09 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.5%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-       vector<int> temp;
-       for(int i=1;i<=n;i++){
-         temp.push_back(i);
-       }
+       if(n==1) return "1";
 
-       for(int i=0;i<k-1;i++){
-          next_permutation(temp.begin(),temp.end());
-       }
+        vector<int> f;
+        int fact=1;
+        f.push_back(fact);
+        
+        for(int i=1;i<=n;i++){
+            fact*=i;
+            f.push_back(fact);
+        }
 
-       string res="";
+        vector<int> v;
+        for(int i=1;i<=n;i++){
+            v.push_back(i);
+        }
 
-       for(auto& it:temp){
-            res+=('0'+it);
-       }
+        string res="";
 
-       return res;
+        rec(n,k,v,f,res);
+
+        return res;
+    }
+
+    void rec(int n,int k,vector<int>& v,vector<int> f,string& res){
+        if(n==1){
+            res+=to_string(v.back());
+            return;
+        }
+
+        int index=(k/f[n-1]);
+        if(k%f[n-1]==0){
+             index--;
+        }
+
+        res+=to_string(v[index]);
+
+        v.erase(v.begin()+index);
+
+        k-=f[n-1]*index;
+
+        rec(n-1,k,v,f,res);
     }
 };
