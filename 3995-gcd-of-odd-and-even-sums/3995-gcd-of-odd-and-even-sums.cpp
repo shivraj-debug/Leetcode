03c5a1e826f774b7bb0959f543c122ae24e6d38b
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-08-28 21:06:11 +0530

    Time: 7 ms (9.07%), Space: 8.4 MB (84.27%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -18,7 +18,19 @@ public:
 
          cout<<even_sum<<" "<<odd_sum;
 
-        return __gcd(odd_sum,even_sum);
+        return find_gcd(even_sum,odd_sum);
 
     }
+
+    int find_gcd(int a,int b){
+        if(a==0) return b;
+        if(b==0) return a;
+
+        if(a==b ) return a;
+
+        if(a>b) return find_gcd(a-b,b);
+
+        return find_gcd(a,b-a);
+    }
+
 };
