5a745dc6ca718f90f049fea17d1ecf383ea0c265
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-09-10 18:56:16 +0530

    Time: 0 ms (100%), Space: 52.4 MB (20.18%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -5,21 +5,16 @@ public:
 
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        dp.resize(n+1,-1);
-        return solve(nums,0); 
-    }
+        int rechable=0;
 
-    bool solve(vector<int>& nums,int i){
-        if(i>=nums.size()-1) return true;
+        for(int i=0;i<n;i++){
+            if(rechable<i){
+                return false;
+            }
 
-        if(nums[i]==0) return false;
-
-        if(dp[i]!=-1) return dp[i];
-
-        for(int l=1;l<=nums[i];l++){
-            if(solve(nums,i+l)) return dp[i]=1;
+            rechable=max(rechable,i+nums[i]);
         }
-
-        return dp[i]=0;
+        return true;
     }
+
 };
