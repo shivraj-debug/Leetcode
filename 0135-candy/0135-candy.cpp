6bcf006b25ce94b9d9d8c1bfb643ac199e142926
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-09-12 15:16:35 +0530

    Time: 0 ms (100%), Space: 21.9 MB (95.56%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
+         int n = ratings.size();
         int candy = n, i=1;
         while(i<n){
             if(ratings[i] == ratings[i-1]){
