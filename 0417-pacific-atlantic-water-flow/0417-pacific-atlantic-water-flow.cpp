4e191e3a5268e8cab94d2d73d4be475b6cf74c4c
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-10-05 10:00:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        vector<vector<int>> ans;
+        int n=heights.size();
+        int m=heights[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                bool left =dfs(i,j,)
+            }
+        }
+
+        return ans;
+    }
+};
