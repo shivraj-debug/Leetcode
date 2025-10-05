e296d3ac128cc83a668a3667c3b88d4fa7918ce1
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-10-05 10:15:40 +0530

    Time: 5 ms (72.28%), Space: 22.2 MB (82.37%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
+    int n, m;
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<int>> ans;
-        int n=heights.size();
-        int m=heights[0].size();
+        n = heights.size();
+        m = heights[0].size();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                bool left =dfs(i,j,)
-            }
+        vector<vector<bool>> pacific(n, vector<bool>(m, false));
+        vector<vector<bool>> atlantic(n, vector<bool>(m, false));
+
+        // DFS from Pacific edges
+        for (int i = 0; i < n; i++) {
+            dfs(i, 0, heights, pacific);        // left column
+            dfs(i, m - 1, heights, atlantic);   // right column
+        }
+        for (int j = 0; j < m; j++) {
+            dfs(0, j, heights, pacific);        // top row
+            dfs(n - 1, j, heights, atlantic);   // bottom row
         }
 
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    ans.push_back({i, j});
+                }
+            }
+        }
         return ans;
+    }
+
+    void dfs(int i, int j, vector<vector<int>>& heights, vector<vector<bool>>& ocean) {
+        ocean[i][j] = true;
+        static int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        for (auto& d : dirs) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+            if (ni < 0 || nj < 0 || ni >= n || nj >= m) continue;
+
+            // only flow to higher or equal height (reverse thinking)
+            if (!ocean[ni][nj] && heights[ni][nj] >= heights[i][j]) {
+                dfs(ni, nj, heights, ocean);
+            }
+        }
     }
 };
