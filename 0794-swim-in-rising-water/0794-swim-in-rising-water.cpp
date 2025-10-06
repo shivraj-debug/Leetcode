303a4e6d17a82ce94a0cabb549b6a0c227b6ffee
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-10-06 08:05:42 +0530

    Time: 5 ms (70.97%), Space: 13.6 MB (61.23%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -7,33 +7,34 @@ public:
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> q;
         q.push({grid[0][0],{0,0}});
 
-        vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
-        vector<vector<int>> vis(n,vector<int>(m,false));
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
 
-        int curr=0;
+        int ans=0;
+
+        vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
 
         while(!q.empty()){
-            auto [mini_time, cell] = q.top();
+            int t=q.top().first;
+            int r=q.top().second.first;
+            int c=q.top().second.second;
+
             q.pop();
-            int x = cell.first, y = cell.second;
 
-            if (vis[x][y]) continue; 
-            vis[x][y] = true;
+            ans=max(t,ans);
+            vis[r][c]=true;
 
-            // update time as the max elevation so far
-            curr = max(curr, mini_time);
+            if(r==n-1 && c==m-1) return ans;
 
-            // if reached bottom-right return
-            if (x == n-1 && y == m-1) return curr;
+            for(int i=0;i<4;i++){
+                int nr=r+dir[i][0];
+                int nc=c+dir[i][1];
 
-            // explore neighbors
-            for (auto &d : dir) {
-                int nx = x + d[0], ny = y + d[1];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny]) {
-                    q.push({grid[nx][ny], {nx, ny}});
+                if(nr<n && nr>=0 && nc<m && nc>=0 && !vis[nr][nc]){
+                    q.push({grid[nr][nc],{nr,nc}});
                 }
             }
         }
-        return curr;
+
+        return ans;
     }
 };
