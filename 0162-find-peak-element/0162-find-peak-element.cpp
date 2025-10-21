154e65a99e79cb4624e5e87da6acd20d074479cd
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-10-21 11:52:44 +0530

    Time: 0 ms (100%), Space: 12.6 MB (34.21%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,10 +7,10 @@ public:
         while(end>start){
             int mid=start+(end-start)/2;
 
-            if(nums[mid]>nums[mid+1]){
-                end=mid;
-            }else{
+            if(nums[mid]<nums[mid+1]){
                 start=mid+1;
+            }else{
+                end=mid;
             }
         }
 
