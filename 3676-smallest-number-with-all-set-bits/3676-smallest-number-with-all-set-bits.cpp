0e6111cbd74381471fdd6ef43d2ebedb5063b122
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-10-29 09:53:58 +0530

    Time: 0 ms (100%), Space: 8.2 MB (60.8%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int ans=0;
+        int x = 1;
 
-        for(int i=n;i<=INT_MAX;i++){
-            bool pre=true;
-            int num=i;
-            while(num>0){
-                int t=num&1;
-                if(t==0){
-                    pre=false;
-                    break;
-                }
-                num=num>>1;              
-            }
-
-            if(pre==true) return i;
+        // Keep doubling x until (x - 1) >= n
+        while ((x - 1) < n) {
+            x <<= 1; // same as x = x * 2
         }
 
-        return -1;
+        return x - 1;
     }
 };
