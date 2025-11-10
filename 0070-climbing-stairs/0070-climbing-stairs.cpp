366e0acdca8ccfb11d49dce24ef2f0a6726606f6
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-11-10 19:28:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> mp(n+1,-1);
-        return helper(n,mp);
+        vector<int> mp(n+1,0);
+
+        mp[0]=1;
+        mp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            mp[i]=mp[i-1]+mp[i-2];
+        }
+
+        return mp[n];
+        
     }
     int helper(int n , vector<int> &mp){
         if (n==0 || n==1) return 1;
