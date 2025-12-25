e6576494da462dd75e0274292d89ae830990ed72
Shiv Raj <136680841+shivraj-debug@users.noreply.github.com>
2025-12-25 15:58:31 +0530

    Time: 0 ms (100%), Space: 12.1 MB (85.14%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        int res = 0;
+        int low=0;
+        int n=citations.size();
+        int high=n;
 
-        sort(citations.begin(), citations.end()); // ascending
+        // if(low>=n) return n;
+        int ans=0;
 
-        for (int i = 0; i < n; i++) {
-            int h = n - i;  // number of papers with at least citations[i]
-            if (citations[i] >= h) {
-                res = h;
-                break; // since sorted ascending, this is the max possible h
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(check(mid,citations)){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
+
         }
 
-        return res;
+        return ans;
+    }
+
+    bool check(int mid,vector<int>& citations){
+        int cnt=0;
+
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]>=mid) cnt++;
+        }
+
+        if(cnt>=mid) return true;
+
+        return false;
     }
 };
